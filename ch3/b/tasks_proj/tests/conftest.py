ca5e27b2a59ba6de3191128b3564e37d5567ed9e
Pavel Druzhinin <paveldroo@gmail.com>
2021-09-10 12:49:56 +0300

    ch3 is finished

--- ch3/b/tasks_proj/tests/conftest.py
+++ ch3/b/tasks_proj/tests/conftest.py
@@ -5,11 +5,12 @@ import tasks
 from tasks import Task
 
 
-@pytest.fixture(scope='session')
-def tasks_db_session(tmpdir_factory):
+# @pytest.fixture(scope='session', params=['tiny'])
+@pytest.fixture(scope='session', params=['tiny', 'mongo'])
+def tasks_db_session(tmpdir_factory, request):
     """Connect to db before tests, disconnect after."""
     temp_dir = tmpdir_factory.mktemp('temp')
-    tasks.start_tasks_db(str(temp_dir), 'tiny')
+    tasks.start_tasks_db(str(temp_dir), request.param)
     yield
     tasks.stop_tasks_db()
 
